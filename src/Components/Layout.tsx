ea8811c1a6c2f2fa127edcb804bd3687702874a2
simisettyharshini@gmail.com <simisettyharshini@gmail.com>
2026-01-25 18:54:39 +0530

    Layout_25012026

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,18 +1,22 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
-
+ 
 const Layout = () => {
   return (
-    <div className="flex min-h-screen">
-      {/* Sidebar */}
+    <div className="fixed inset-0 overflow-hidden">
+      <div className="flex h-screen w-screen overflow-hidden">
+      {/* Sidebar (only scrollable area) */}
       <Sidebar />
-
-      {/* Page Content */}
-      <div className="flex-1 bg-[#020817] p-6 overflow-auto">
-        <Outlet />
+ 
+      {/* Main content (NO SCROLL EVER) */}
+      <div className="flex-1 bg-[#020817] overflow-hidden">
+        <div className="h-full w-full pt-4 pl-5 pr-6 box-border overflow-hidden">
+          <Outlet />
+        </div>
       </div>
+    </div>
     </div>
   );
 };
-
+ 
 export default Layout;
